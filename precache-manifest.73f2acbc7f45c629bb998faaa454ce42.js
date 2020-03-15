fe7db04657450173559d7bd31d32994d696f7686
SDubaj <dubajsebastian96@gmail.com>
2020-03-15 12:43:20 +0100

    Updates

---
+++ precache-manifest.73f2acbc7f45c629bb998faaa454ce42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "599356268b50d8c1f0c39c8b9d771b60",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "ffd996136a09155a87e2",
+    "url": "/Calculator/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e7c238458af6d5a3f4c5",
+    "url": "/Calculator/static/css/main.2c3bdb09.chunk.css"
+  },
+  {
+    "revision": "ffd996136a09155a87e2",
+    "url": "/Calculator/static/js/2.c1a38a89.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Calculator/static/js/2.c1a38a89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7c238458af6d5a3f4c5",
+    "url": "/Calculator/static/js/main.8d347020.chunk.js"
+  },
+  {
+    "revision": "530ad879f983d30c296d",
+    "url": "/Calculator/static/js/runtime-main.ab83f4e4.js"
+  }
+]);
